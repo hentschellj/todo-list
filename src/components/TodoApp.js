0e1aaa589fa2d8b54c2c7c1d7f4ab51e771db750
hentschellj <hentschellj@gmail.com>
2020-11-19 16:51:35 -0600

    Set initial state

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,6 +3,16 @@ import NewItem from './NewItem'
 import Todo from './Todo'
 
 export default class TodoApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [
+        'Get Groceries',
+        'Feed Cat',
+        'Code Website'
+      ]
+    }
+  }
   render() {
     return (
       <div>
