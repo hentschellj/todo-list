88480ebfe3a5f63f1f5ccad2933e972beb44c542
hentschellj <hentschellj@gmail.com>
2020-11-19 17:03:13 -0600

    Created basic input field

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 export default class NewItem extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+
+    this.updateText = this.updateText.bind(this);
+  }
+
+  updateText(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
   render() {
     return (
-      <div>New Item</div>
+      <div>
+        {this.state.text}
+        <input onChange={this.updateText} type="text" />
+        <button>Add Item</button>
+      </div>
     )
   }
 }
